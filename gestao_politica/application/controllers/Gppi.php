463320c1b559d51daa3c0c17d4bd7782071adeba
Manoel Carvalho Neto <manoel.carvalho.neto@gmail.com>
2018-08-30 14:19:49 -0300

    Metodo para testes

--- gestao_politica/application/controllers/Gppi.php
+++ gestao_politica/application/controllers/Gppi.php
@@ -20,8 +20,46 @@ class Gppi extends CI_Controller {
         ini_set("memory_limit", "-1");
 
         $this->load->model('GPPI_Model');
+        echo "------ Idosos ------";
         var_dump($this->GPPI_Model->get_beneficiarios_por_idoso());
         var_dump($this->GPPI_Model->count_familias_pessoas_return_object($this->GPPI_Model->get_beneficiarios_por_idoso()));
+        echo "<br><br>";
+        echo "------ Crianças ------";
+        var_dump($this->GPPI_Model->get_beneficiarios_por_crianca());
+        var_dump($this->GPPI_Model->count_familias_pessoas_return_object($this->GPPI_Model->get_beneficiarios_por_crianca()));
+        echo "<br><br>";
+        echo "------ Faixa Etária ------";
+        var_dump($this->GPPI_Model->get_beneficiarios_por_faixa_etaria_pessoa(8, 50));
+        var_dump($this->GPPI_Model->count_familias_pessoas_return_object($this->GPPI_Model->get_beneficiarios_por_faixa_etaria_pessoa(8, 50)));
+        echo "<br><br>";
+        echo "------ Idade Pessoa ------";
+        var_dump($this->GPPI_Model->get_beneficiarios_por_idade_pessoa(27, '>='));
+        var_dump($this->GPPI_Model->count_familias_pessoas_return_object($this->GPPI_Model->get_beneficiarios_por_idade_pessoa(27, '>=')));
+        echo "<br><br>";
+        echo "------ Raça ou Cor ------";
+        var_dump($this->GPPI_Model->get_beneficiarios_por_raca(2));
+        var_dump($this->GPPI_Model->count_familias_pessoas_return_object($this->GPPI_Model->get_beneficiarios_por_raca(2)));
+        echo "<br><br>";
+        echo "------ Sexo ------";
+        var_dump($this->GPPI_Model->get_beneficiarios_por_sexo(1));
+        var_dump($this->GPPI_Model->count_familias_pessoas_return_object($this->GPPI_Model->get_beneficiarios_por_sexo(1)));
+        echo "<br><br>";
+        echo "------ Renda Familia ------";
+        var_dump($this->GPPI_Model->get_beneficiarios_por_renda_familia(2400, '>='));
+        var_dump($this->GPPI_Model->count_familias_pessoas_return_object($this->GPPI_Model->get_beneficiarios_por_renda_familia(2400, '>=')));
+        echo "<br><br>";
+        echo "------ Renda Pessoa ------";
+        var_dump($this->GPPI_Model->get_beneficiarios_por_renda_pessoa(1500, '>='));
+        var_dump($this->GPPI_Model->count_familias_pessoas_return_object($this->GPPI_Model->get_beneficiarios_por_renda_pessoa(1500, '>=')));
+        echo "<br><br>";
+        echo "------ Cep ------";
+        var_dump($this->GPPI_Model->get_beneficiarios_por_cep(1, 1, '80010130'));
+        var_dump($this->GPPI_Model->count_familias_pessoas_return_object($this->GPPI_Model->get_beneficiarios_por_cep(1, 1, '80010130')));
+        echo "<br><br>";
+        echo "------ Bairro ------";
+        var_dump($this->GPPI_Model->get_beneficiarios_por_bairro(1, 1, 'Bigorrilho'));
+        var_dump($this->GPPI_Model->count_familias_pessoas_return_object($this->GPPI_Model->get_beneficiarios_por_bairro(1, 1, 'Bigorrilho')));
+        echo "<br><br>";
     }
 
 }
