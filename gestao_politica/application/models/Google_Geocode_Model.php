247bb2788659a303ad7f264033987206e4700f6c
Manoel Carvalho Neto <manoel.carvalho.neto@gmail.com>
2018-09-11 12:00:26 -0300

    Model para a função de geolocalização do google maps api.

---
+++ gestao_politica/application/models/Google_Geocode_Model.php
@@ -0,0 +1,47 @@
+<?php
+
+class GPPI_Model extends CI_Model {
+
+    public function geocode($address) {
+
+        // url encode the address
+        $address = urlencode($address);
+
+        // google map geocode api url
+        $url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=YOUR_API_KEY";
+
+        // get the json response
+        $resp_json = file_get_contents($url);
+
+        // decode the json
+        $resp = json_decode($resp_json, true);
+
+        // response status will be 'OK', if able to geocode given address 
+        if ($resp['status'] == 'OK') {
+
+            // get the important data
+            $lati = isset($resp['results'][0]['geometry']['location']['lat']) ? $resp['results'][0]['geometry']['location']['lat'] : "";
+            $longi = isset($resp['results'][0]['geometry']['location']['lng']) ? $resp['results'][0]['geometry']['location']['lng'] : "";
+            $formatted_address = isset($resp['results'][0]['formatted_address']) ? $resp['results'][0]['formatted_address'] : "";
+
+            // verify if data is complete
+            if ($lati && $longi && $formatted_address) {
+
+                // put the data in the array
+                $data_arr = array();
+                array_push($data_arr, $lati, $longi, $formatted_address);
+                return $data_arr;
+            } else {
+                return false;
+            }
+        } else {
+            echo "<strong>ERROR: {$resp['status']}</strong>";
+            return false;
+        }
+    }
+
+    public function get_api_key() {
+        
+    }
+
+}
