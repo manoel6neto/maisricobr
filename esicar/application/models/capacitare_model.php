78559d6f8908e5abe648752576d7e2d6a2ffb8be
Manoel Carvalho Neto <manoel.carvalho.neto@gmail.com>
2018-09-26 12:14:42 -0300

    Ajuste Layout Capacitare

--- esicar/application/models/capacitare_model.php
+++ esicar/application/models/capacitare_model.php
@@ -13,6 +13,33 @@ class capacitare_model extends CI_Model {
         return NULL;
     }
 
+    public function remove_cadastro($id) {
+        $this->db->where('id', $id);
+        $this->db->delete('capacitare');
+    }
+
+    public function get_from_email($email) {
+        $this->db->where('email', $email);
+        $query = $this->db->get('capacitare');
+
+        if ($query->num_rows > 0) {
+            return $query->result();
+        }
+
+        return NULL;
+    }
+
+    public function get_from_celular($celular) {
+        $this->db->where('telefone', $celular);
+        $query = $this->db->get('capacitare');
+
+        if ($query->num_rows > 0) {
+            return $query->result();
+        }
+
+        return NULL;
+    }
+
     public function get_all_by_evento($evento_id) {
         $this->db->flush_cache();
         $this->db->distinct();
@@ -27,6 +54,7 @@ class capacitare_model extends CI_Model {
 
     public function get_eventos() {
         $this->db->flush_cache();
+        $this->db->order_by('data_evento', 'desc');
         $query = $this->db->get('capacitare_evento');
         if ($query->num_rows > 0) {
             return $query->result();
@@ -55,6 +83,22 @@ class capacitare_model extends CI_Model {
         }
 
         return NULL;
+    }
+
+    public function set_evento_ativo($id_evento) {
+        //Removendo os ativos
+        $this->db->set('ativo', 0);
+        $this->db->update('capacitare_evento');
+
+        //Ativando o novo como ativo
+        $this->db->where('id', $id_evento);
+        $this->db->set('ativo', 1);
+        $this->db->update('capacitare_evento');
+    }
+
+    public function insert_evento($options) {
+        $this->db->insert('capacitare_evento', $options);
+        return $this->db->affected_rows();
     }
 
     public function insert_data($email, $telefone) {
--- esicar/application/views/capacitare/principal.php
+++ esicar/application/views/capacitare/principal.php
@@ -1,18 +1,21 @@
 <div class="capacitare_principal">
     <div class="box_principal">
-        <h3 style="text-align: center; margin-bottom: 50px;">CAPACITARE - GESTÃO DE HOTSPOT</h3>
+        <h2 style="text-align: center; padding-bottom: 3%;">GESTÃO DE HOTSPOT</h2>
         <form id="principal" method="post">
-            <select class="select_capacitare" id="eventos" name="eventos">
-                <option value="0">Todos</option>
-                <?php foreach ($eventos as $evento): ?>
-                    <option value="<?php echo $evento->id; ?>"><?php echo $evento->nome . ' - ' . $model->format_data_date($evento->data_evento); ?></option>
-                <?php endforeach; ?>
-            </select>
-            <div class="buttons_div" style="float: left; margin-left: 5%;">
-                <input style="width: 150px; padding: 5px; border-radius: 5px; background: #167F92; color: #FFF; height: 100px; font-size: 25px; font-weight: 900;" id="acao_email" type="submit" name="acao" value="EMAIL"/>
+            <div class="form-row" style="padding: 3%;">
+                <select class="select_capacitare" id="eventos" name="eventos">
+                    <option value="0">Todos os eventos</option>
+                    <?php foreach ($eventos as $evento): ?>
+                        <option value="<?php echo $evento->id; ?>"><?php echo $evento->nome . ' - ' . $model->format_data_date($evento->data_evento); ?></option>
+                    <?php endforeach; ?>
+                </select>
             </div>
-            <div class="buttons_div" style="float: right; margin-right: 5%;">
-                <input style="width: 150px; padding: 5px; border-radius: 5px; background: #167F92; color: #FFF; height: 100px; font-size: 25px; font-weight: 900;" id="acao_sms" type="submit" name="acao" value="SMS"/>
+            <div class="form-row centralizar" style="padding: 5% 0 5% 0;">
+                <div class="btn-group btn-group-lg" role="group" aria-label="Módulos">
+                    <button id="acao_evento" type="submit" name="acao_evento" class="btn btn-secondary btn-capacitare">EVENTOS</button>
+                    <button id="acao_sms" type="submit" name="acao_sms" class="btn btn-secondary btn-capacitare">SMS</button>
+                    <button id="acao_email" type="submit" name="acao_email" class="btn btn-secondary btn-capacitare">E-MAIL</button>
+                </div>
             </div>
         </form>
     </div>
