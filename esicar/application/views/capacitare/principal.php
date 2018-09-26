36ce48c88f3d8ab652b0418c10ac9161c158a7d8
Manoel Carvalho Neto <manoel.carvalho.neto@gmail.com>
2018-09-26 12:50:53 -0300

    Finalizado o layout da tela principal da capacitare.

--- esicar/application/views/capacitare/principal.php
+++ esicar/application/views/capacitare/principal.php
@@ -1,16 +1,16 @@
 <div class="capacitare_principal">
     <div class="box_principal">
-        <h2 style="text-align: center; padding-bottom: 3%;">GESTÃO DE HOTSPOT</h2>
+        <h2 style="text-align: center; padding-bottom: 4%;"><u>GESTÃO DE HOTSPOT</u></h2>
         <form id="principal" method="post">
-            <div class="form-row" style="padding: 3%;">
+            <div class="form-row centralizar" style="padding: 4% 0 4% 0;">
                 <select class="select_capacitare" id="eventos" name="eventos">
-                    <option value="0">Todos os eventos</option>
+                    <option class="select_capacitare" value="0">Todos os eventos</option>
                     <?php foreach ($eventos as $evento): ?>
-                        <option value="<?php echo $evento->id; ?>"><?php echo $evento->nome . ' - ' . $model->format_data_date($evento->data_evento); ?></option>
+                        <option class="select_capacitare" value="<?php echo $evento->id; ?>"><?php echo $evento->nome . ' '; ?><?= $evento->ativo == 1 ? "(Ativo)" : ""; ?></option>
                     <?php endforeach; ?>
                 </select>
             </div>
-            <div class="form-row centralizar" style="padding: 5% 0 5% 0;">
+            <div class="form-row centralizar" style="padding: 4% 0 4% 0;">
                 <div class="btn-group btn-group-lg" role="group" aria-label="Módulos">
                     <button id="acao_evento" type="submit" name="acao_evento" class="btn btn-secondary btn-capacitare">EVENTOS</button>
                     <button id="acao_sms" type="submit" name="acao_sms" class="btn btn-secondary btn-capacitare">SMS</button>
--- esicar/application/views/template_capacitare.php
+++ esicar/application/views/template_capacitare.php
@@ -13,7 +13,7 @@
         <script type="text/javascript">	//<![CDATA[urlApp = '<?= base_url(); ?>';//]]></script>
     </head>
 
-    <body class="loginWrapper" style="padding: 2%;">
+    <body class="loginWrapper" style="padding: 4%;">
         <?php $this->load->view($main); ?>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
